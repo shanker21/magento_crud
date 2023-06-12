e146e7bd87db7299c4677fffb800e3f15025b36e
shanker21 <54769784+shanker21@users.noreply.github.com>
2023-06-12 15:56:48 +0530

    Update Login.php
    added loginin

--- WebArchers/PDFInvoice/Controller/Index/Login.php
+++ WebArchers/PDFInvoice/Controller/Index/Login.php
@@ -8,6 +8,7 @@ use WebArchers\PDFInvoice\Model\PdfinvoiceFactory;
 use Magento\Framework\Controller\Result\Redirect;
 use Magento\Framework\Controller\Result\RedirectFactory;
 use Magento\Framework\Session\SessionManagerInterface;
+
 class Login extends Action
 {
     protected $_pageFactory;
@@ -31,6 +32,18 @@ class Login extends Action
         }
         public function execute()
         {
+            // Start the session
+        $this->sessionManager->start();
+             // Check if the user is already logged in
+             $isLoggedIn = $this->sessionManager->isLoggedIn();
+
+
+            if ($isLoggedIn) {
+                // User is already logged in, redirect to the welcome page
+                $redirect = $this->resultRedirectFactory->create();
+                $redirect->setPath('pdfinvoice/index/welcome');
+                return $redirect;
+            }
                 $params = $this->getRequest()->getParams();
     
                 // Retrieve the email and password values from the request parameters
@@ -41,11 +54,15 @@ class Login extends Action
                 // Load the model by email
                 $model = $this->pdfinvoiceFactory->create();
                 $model->load($email, 'email_id');
+
+                // If the login is successful
                 if ($model->getId() && $model->getPassword() === $password) {
                     $firstName = $model->getFirstName();
                     $lastName = $model->getLastName();
 
                     //store the value in session
+                    // Store the login status in the session
+                        
                     $this->sessionManager->setFirstName($firstName);
                     $this->sessionManager->setLastName($lastName);
             
